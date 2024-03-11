9dc8a54d489f95146b5b8d9a904d42a5c628fccf
Priyanshu Anand <anandpriyanshu6@gmail.com>
2024-03-12 02:32:00 +0530

    Added Trace Id in Api Gateway

--- probank-apigateway/src/main/java/com/probank/probankapigateway/configs/GlobalConfigurations.java
+++ probank-apigateway/src/main/java/com/probank/probankapigateway/configs/GlobalConfigurations.java
@@ -1,5 +1,7 @@
 package com.probank.probankapigateway.configs;
 
+import java.time.LocalDateTime;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,22 +12,26 @@ public class GlobalConfigurations {
 
 	@Bean
 	RouteLocator getCustomRouteLocator(RouteLocatorBuilder builder) {
-		
-		//WE ADDED CUSTOM PREFIX ALSO "/probank" BEFORE EVERY URL STARTS AND WRITE THE LOGIC TO TRANSFER IT TO DIFFERENT MS
-		
+
+		// WE ADDED CUSTOM PREFIX ALSO "/probank" BEFORE EVERY URL STARTS AND WRITE THE
+		// LOGIC TO TRANSFER IT TO DIFFERENT MS
+
 		return builder.routes()
-				.route(p -> 
-						p.path("/probank/probank-accounts-service/**")
-						 .filters(f -> f.rewritePath("/probank/probank-accounts-service/?(?<remaining>.*)", "/${remaining}"))
-						 .uri("lb://PROBANK-ACCOUNTS-SERVICE"))
-				.route(p -> 
-						p.path("/probank/probank-loans-service/**")
-						 .filters(f -> f.rewritePath("/probank/probank-loans-service/?(?<remaining>.*)", "/${remaining}"))
-						 .uri("lb://PROBANK-LOANS-SERVICE"))
-				.route(p -> 
-						p.path("/probank/probank-cards-service/**")
-						 .filters(f -> f.rewritePath("/probank/probank-cards-service/?(?<remaining>.*)", "/${remaining}"))
-						 .uri("lb://PROBANK-CARDS-SERVICE"))
+				.route(p -> p.path("/probank/probank-accounts-service/**")
+						.filters(f -> f.rewritePath("/probank/probank-accounts-service/?(?<remaining>.*)", "/${remaining}")
+								.addRequestHeader("X-request-start-time", LocalDateTime.now().toString())
+								.addResponseHeader("X-request-end-time", LocalDateTime.now().toString()))
+						.uri("lb://PROBANK-ACCOUNTS-SERVICE"))
+				.route(p -> p.path("/probank/probank-loans-service/**")
+						.filters(f -> f.rewritePath("/probank/probank-loans-service/?(?<remaining>.*)", "/${remaining}")
+								.addRequestHeader("X-request-start-time", LocalDateTime.now().toString())
+								.addResponseHeader("X-request-end-time", LocalDateTime.now().toString()))
+						.uri("lb://PROBANK-LOANS-SERVICE"))
+				.route(p -> p.path("/probank/probank-cards-service/**")
+						.filters(f -> f.rewritePath("/probank/probank-cards-service/?(?<remaining>.*)", "/${remaining}")
+								.addRequestHeader("X-request-start-time", LocalDateTime.now().toString())
+								.addResponseHeader("X-request-end-time", LocalDateTime.now().toString()))
+						.uri("lb://PROBANK-CARDS-SERVICE"))
 				.build();
 	}
 }
