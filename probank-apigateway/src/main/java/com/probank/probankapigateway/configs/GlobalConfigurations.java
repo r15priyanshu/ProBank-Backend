fa490fd7c6f99f864a5f57a089eee0eda281b9ec
Priyanshu Anand <anandpriyanshu6@gmail.com>
2024-03-20 23:56:59 +0530

    Example to add Timeout in Api Gateway Without Circuit Breaker

--- probank-apigateway/src/main/java/com/probank/probankapigateway/configs/GlobalConfigurations.java
+++ probank-apigateway/src/main/java/com/probank/probankapigateway/configs/GlobalConfigurations.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.RouteMetadataUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,11 @@ public class GlobalConfigurations {
 		// WE ADDED CUSTOM PREFIX ALSO "/probank" BEFORE EVERY URL STARTS AND WRITE THE
 		// LOGIC TO TRANSFER IT TO DIFFERENT MS
 
+		// NOTE : If you are using circuit breaker then metadata timeout will not work ,
+		// if you are not using circuit breaker then it will work because circuit
+		// breaker uses its own timeout [ You can change that if you want , bydefault it
+		// will be 1000ms ]
+
 		return builder.routes().route(p -> p.path("/probank/probank-accounts-service/**")
 				.filters(f -> f.rewritePath("/probank/probank-accounts-service/?(?<remaining>.*)", "/${remaining}")
 						.addRequestHeader("X-request-start-time", LocalDateTime.now().toString())
@@ -23,7 +29,8 @@ public class GlobalConfigurations {
 						.circuitBreaker(config -> {
 							config.setName("accountsCircuitBreaker");
 							config.setFallbackUri("forward:/fallBackForAccountsMicroservice");
-						}))
+						}).metadata(RouteMetadataUtils.CONNECT_TIMEOUT_ATTR, 1000)
+						.metadata(RouteMetadataUtils.RESPONSE_TIMEOUT_ATTR, 5000))
 				.uri("lb://PROBANK-ACCOUNTS-SERVICE"))
 				.route(p -> p.path("/probank/probank-loans-service/**")
 						.filters(f -> f.rewritePath("/probank/probank-loans-service/?(?<remaining>.*)", "/${remaining}")
